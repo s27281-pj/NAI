39082479a428d9edf94f1db82d9bbde1ba80867e
Roland Liedtke <s28961@pjwstk.edu.pl>
2025-11-16 11:32:17 +0100

    dodaje dane treningowe, porządkuje strukture katalogów, dodaje program konwertujący oceny filmów

---
+++ wyklad3/ratings_convert.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+# === 1. Wczytanie danych z CSV ===
+df = pd.read_csv("data_train/ratings.csv")
+# print(df.head())
+
+
+# === 2. Dodanie przeskalowanej oceny 0–10 ===
+# mnożenie ×2 i zmiana na całkowite liczby
+df["rating_10"] = (df["rating"] * 2).astype(int)
+
+
+# # === 3. Zapis wyniku do pliku ===
+df.to_csv("data_train/ratings_10.csv", index=False)
+
+print("Gotowe! Zapisano jako: filmy_z_ocenami_10.csv")
